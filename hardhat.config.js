26b2b1fb07018a23fc454cc1096354256a782ece
Su Myat Aung <47875348+sumyathtethtet@users.noreply.github.com>
2024-03-12 12:59:05 +0000

    Add files via upload

---
+++ hardhat.config.js
@@ -0,0 +1,12 @@
+require("@nomicfoundation/hardhat-toolbox");
+require("hardhat-deploy")
+
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  solidity: "0.8.24",
+  namedAccounts: {
+    deployer: {
+        default: 0, // ethers built in account at index 0
+    },
+},
+};
